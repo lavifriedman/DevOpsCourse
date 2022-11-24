dce917a6957e58da21a5eb4d76b324175adf3a1e
lavi.friedman <lavi.friedman@nitzanim.tech>
2022-11-24 10:19:24 +0000

    I solve this exercise

---
+++ python/24.11/debag_exercise/main.py
@@ -0,0 +1,81 @@
+# EX5: R2D2000 Brain.
+result = ''
+booly = True
+count_for_fun = 0
+
+if booly * 7:
+    booly = booly != booly
+    if booly or True != booly:
+        if True + (booly or False) - booly:
+            if True * 2 - booly * 3:
+                booly = booly <= booly
+                if booly != True:
+                    result += 'R2D1000 '
+                    booly = booly != booly
+                    count_for_fun -= 1
+                elif booly and booly == 1 or booly - booly:
+                    result += 'R2D2000 '
+                    count_for_fun += 1
+                if 3 ** 2 / 10 ** 2 * -5 // (1 / 100) % 2 == 0:
+                    count_for_fun += 1
+            if booly == True:
+                result += 'is '
+                booly = booly <= booly
+                count_for_fun += 1
+            booly = booly != booly
+        if booly == False:
+            if booly == booly:
+                if booly == booly:
+                    if booly != booly:
+                        if booly == True + booly or booly:
+                            if booly > (booly - 1) and booly < 1:
+                                count_for_fun += 1
+                        else:
+                            count_for_fun += 1
+                    else:
+                        result += 'ALL '
+                        count_for_fun += 1
+                elif booly != booly:
+                    result += 'far from '
+                    count_for_fun -= 1
+                if booly == booly:
+                    result += 'GOOD! '
+                    count_for_fun += 1
+            else:
+                count_for_fun += 1
+        elif booly and -1 + booly ** 3 or booly % 2 != 1:
+            count_for_fun += 1
+if True:
+    if True:
+        if True:
+            if True:
+                if True:
+                    result += 'Ready '
+                    count_for_fun += 1
+        else:
+            if booly or booly <= booly and booly < 1:
+                if booly - booly ** booly < 1:
+                    if 3 ** 2 / 10 ** 2 * -5 // ((1 and False + True / 100) % 2) != 0:
+                        result += 'I '
+                        count_for_fun -= 1
+elif booly == booly - booly:
+    pass
+if True:
+    if True:
+        result += 'to '
+        count_for_fun += 1
+    elif booly or booly <= booly and booly < 1:
+        if booly > 1 / count_for_fun:
+            result += 'will never '
+            count_for_fun -= 1
+        else:
+            count_for_fun += 1
+    if True:
+        result += 'work! :)'
+        count_for_fun += 1
+    elif 4 ** 3 >= -2 * count_for_fun or 9 * 10 ** 3 * 16 < (1 / 2 and 9000) % 4 != 0:
+        result += 'work! :('
+        count_for_fun += 1
+
+print("Result:", result)
+
