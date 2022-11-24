52a7d08e107194621a631ac67917f33b598eff90
lavi.friedman <lavi.friedman@nitzanim.tech>
2022-11-24 14:00:04 +0000

    i start a function that count the numbers neighbors.

---
+++ python/24.11/exercise_os_module/main.py
@@ -0,0 +1,6 @@
+import os
+import camelcase
+
+print(os.getcwd())
+os.chdir('/users/devops/Desktop')
+os.mkdir('testone')
--- python/24.11/finish_exercise/main.py
+++ python/24.11/finish_exercise/main.py
@@ -1,22 +1,50 @@
-def create_squares(length, height, squares):
-    #create the square of sells
-    list_len = []
-    while len(list_len) < length:
-        list_len.append("ded")
-    while len(squares) < height:
-        squares.append(list_len)
+def create_squares(length, height):
+    squares = []
+    for n in range(height):
+        squares.append([])
+    for cell_line in squares:
+        for new_cell in range(length):
+            cell_line.append(None)
     return squares
 
-#def put_the_first_alive_sell(length_location, height_location, squares):
-   # squares[length_location][height_location] = "alive"
-
-def main():
-    squares = []
-    create_squares(100, 100, squares)
-    #put_the_first_alive_sell(0, 0, squares)
-    squares[0][0] = "alive"
+def print_the_squares(squares):
     for n in squares:
         print(n)
+def set_alive_cells(row, height, squares):
+    squares[row][height] = "alive"
+
+def set_ded_cells(row, height, squares):
+    squares[row][height] = None
+
+def count_number_of_neighbors(row, height, squares):
+    neighbors_number = 0
+    if squares[row - 1][height - 1] == 'alive':
+        neighbors_number += 1
+    if squares[row - 1][height] == 'alive':
+        neighbors_number += 1
+    if squares[row - 1][height + 1] == 'alive':
+        neighbors_number += 1
+    if squares[row ][height - 1] == 'alive':
+        neighbors_number += 1
+    if squares[row][height + 1] == 'alive':
+        neighbors_number += 1
+    if squares[row + 1][height - 1] == 'alive':
+        neighbors_number += 1
+    if squares[row + 1][height] == 'alive':
+        neighbors_number += 1
+    if squares[row + 1][height + 1] == 'alive':
+        neighbors_number += 1
+    return neighbors_number
+
+
+def main():
+    squares = create_squares(100,100)
+    set_alive_cells(20,20,squares)
+    set_alive_cells(21,20,squares)
+    set_alive_cells(20,21,squares)
+    print(count_number_of_neighbors(20,20,squares))
+    print_the_squares(squares)
+
 
 if __name__ == "__main__":
     main()
