4c6a264dc9c1ba092d448811ab93db69a5bbe194
lavi.friedman <lavi.friedman@nitzanim.tech>
2022-12-08 15:07:04 +0000

    add a function for start the game

--- python/24.11/finish_exercise/main.py
+++ python/24.11/finish_exercise/main.py
@@ -21,34 +21,62 @@ def set_ded_cells(row, height, squares):
     squares[row][height] = None
 
 
-def count_number_of_neighbors(row, height, squares):
+def count_number_of_neighbors(row, colum, squares):
     neighbors_number = 0
-    if squares[row - 1][height - 1] == 'alive':
+    if (cells_is_not_out_of_squares_range(row - 1, colum - 1, squares)) and (squares[row - 1][colum - 1] == 'alive'):
         neighbors_number += 1
-    if squares[row - 1][height] == 'alive':
+    if (cells_is_not_out_of_squares_range(row - 1, colum, squares)) and (squares[row - 1][colum] == 'alive'):
         neighbors_number += 1
-    if squares[row - 1][height + 1] == 'alive':
+    if (cells_is_not_out_of_squares_range(row - 1, colum + 1, squares)) and (squares[row - 1][colum + 1] == 'alive'):
         neighbors_number += 1
-    if squares[row ][height - 1] == 'alive':
+    if (cells_is_not_out_of_squares_range(row, colum - 1, squares)) and (squares[row][colum - 1] == 'alive'):
         neighbors_number += 1
-    if squares[row][height + 1] == 'alive':
+    if (cells_is_not_out_of_squares_range(row, colum + 1, squares)) and (squares[row][colum + 1] == 'alive'):
         neighbors_number += 1
-    if squares[row + 1][height - 1] == 'alive':
+    if (cells_is_not_out_of_squares_range(row + 1, colum - 1, squares)) and (squares[row + 1][colum - 1] == 'alive'):
         neighbors_number += 1
-    if squares[row + 1][height] == 'alive':
+    if (cells_is_not_out_of_squares_range(row + 1, colum, squares)) and (squares[row + 1][colum] == 'alive'):
         neighbors_number += 1
-    if squares[row + 1][height + 1] == 'alive':
+    if (cells_is_not_out_of_squares_range(row + 1, colum + 1, squares)) and (squares[row + 1][colum + 1] == 'alive'):
         neighbors_number += 1
     return neighbors_number
 
 
+def cells_is_not_out_of_squares_range(row, colum, squares):
+    if (row < 0) or (colum < 0) or (len(squares) < row) or (len(squares[0]) < colum):
+        return False
+    return True
+
+
+def let_the_user_set_all_the_alive_cells(squares):
+    message_for_the_player = """Welcome to the game of life.
+    put the locations of where you want put a alive cell over the squares board.
+    for start the the hunter games enter 'start'.
+    """
+    user_finish = False
+    print(message_for_the_player)
+    while not user_finish:
+        user_input = input(': ')
+        if user_input == 'start':
+            user_finish = True
+            continue
+        cell_location = user_input.split(',')
+        try:
+            cell_location[0] = int(cell_location[0])
+            cell_location[1] = int(cell_location[1])
+        except ValueError:
+            print('we put a illegal input, please put a new legal input.')
+            continue
+        if not cells_is_not_out_of_squares_range(cell_location[0], cell_location[1], squares):
+            print('the location you enter is out of range, please choose ather location.')
+            continue
+        set_alive_cells(cell_location[0], cell_location[1],squares)
+    return squares
+
+
 def main():
-    squares = create_squares(100,100)
-    set_alive_cells(20,20,squares)
-    set_alive_cells(21,20,squares)
-    set_alive_cells(20,21,squares)
-    print(count_number_of_neighbors(20,20,squares))
-    print_the_squares(squares)
+    squares = create_squares(10, 10)
+    let_the_user_set_all_the_alive_cells(squares)
 
 
 if __name__ == "__main__":
