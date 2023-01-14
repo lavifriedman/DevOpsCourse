ffb55652cd4b57eceac4aaecc5f5fe05833032e6
lavifriedman <lavi.friedman@nitzanim.tech>
2023-01-14 21:40:49 +0200

    create a hangman python game

--- python/camposil-self-py-course/self course/venv/Tasks/Hangman/Hangman/Hangman-lavi-friedman.py
+++ python/camposil-self-py-course/self course/venv/Tasks/Hangman/Hangman/Hangman-lavi-friedman.py
@@ -2,6 +2,7 @@ def hangman_pictures():
     picture1 = """   x-------x"""
 
     picture2 = """
+    :(
         x-------x
         |
         |
@@ -11,6 +12,7 @@ def hangman_pictures():
 
     """
     picture3 = """
+    :(
         x-------x
         |       |
         |       0
@@ -20,6 +22,7 @@ def hangman_pictures():
 
     """
     picture4 = """ 
+    :(
         x-------x
         |       |
         |       0
@@ -28,7 +31,8 @@ def hangman_pictures():
         |
 
     """
-    picture5 = """
+    picture5 = r"""
+    :(
         x-------x
         |       |
         |       0
@@ -37,7 +41,8 @@ def hangman_pictures():
         |
 
     """
-    picture6 = """
+    picture6 = r"""
+    :(
         x-------x
         |       |
         |       0
@@ -46,7 +51,8 @@ def hangman_pictures():
         |
 
     """
-    picture7 = """
+    picture7 = r"""
+    :(
         x-------x
         |       |
         |       0
@@ -59,10 +65,12 @@ def hangman_pictures():
     HANGMAN_PHOTOS = (picture1, picture2, picture3, picture3, picture4, picture5, picture6, picture7)
     return HANGMAN_PHOTOS
 
+
 def print_hangman():
     """Function print a huge output of 'Hangman'.
      :return: None"""
-    print("""  _    _                                         
+    print(r"""  
+      _    _                                         
      | |  | |                                        
      | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __  
      |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
@@ -87,7 +95,7 @@ def check_win(secret_word, old_letters_guessed):
 
 
 def show_hidden_word(secret_word, old_letters_guessed):
-    """function receive a string of word and a list of letters,
+    """Function receive a string of word and a list of letters,
     and find which letters from the list are all ready located in the string.
     :param secret_word: a string of word.
     :param old_letters_guessed: list of old letters guess.
@@ -99,58 +107,47 @@ def show_hidden_word(secret_word, old_letters_guessed):
     for letter in secret_word:
         if letter in old_letters_guessed:
             word_status = word_status + letter + " "
-        else :
+        else:
             word_status = word_status + "_ "
     return word_status
 
 
 def check_valid_input(letter_guessed, old_letters_guessed):
-    """check if the letter_guessed is a single letter from the english alphabet.
+    """Check if the letter_guessed is a single letter from the english alphabet.
     :param letter_guessed: the user input guessed
-    :param old_letters_guessed: list of letters the player all radey use.
+    :param old_letters_guessed: list of letters the player all ready use.
     :type letter_guessed: str
     :type old_letters_guessed: list
-    :return: True/False if letter_guessed is a singl letter
+    :return: True/False if letter_guessed is a single letter
     :rtype: bool.
     """
-    english_alphabet = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
-    letter_guessed_is_single_sing = len(letter_guessed) == 1
-    letter_guessed_is_letter = False
+    letter_guessed_is_single_letter = (len(letter_guessed) == 1) and (letter_guessed.isalpha())
+    # check if letter is single english letter.
+    if not letter_guessed_is_single_letter:
+        return False
     # check if letter_guessed is a letter the player all ready use.
     for letter in old_letters_guessed:
         if letter_guessed.lower() == letter:
             return False
-    # check if letter_guessed is a letter in english alphabet
-    for letter in english_alphabet:
-        if letter == letter_guessed:
-            letter_guessed_is_letter = True
-            break
-    # check if the letter_guessed is a single letter from the englihs alphabet
-    if letter_guessed_is_single_sing and letter_guessed_is_letter:
-        return True
-    else:
-        return False
+    return True
 
 
 def try_update_letter_guessed(letter_guessed, old_letters_guessed):
-    """the function check if the player guess is a single letter that was not guessed before.
+    """The function check if the player guess is a single letter that was not guessed before.
     :param letter_guessed: string that present the player guess.
     :param old_letters_guessed: list that contain all the letters the player all ready guessed
-    :type letter_guessed: str.
-    :type old_letters_guessed: list.
-    :return: True/False if letter_guessed is a singel letter that wasnt guessed before.
+    :type letter_guessed: str
+    :type old_letters_guessed: list
+    :return: True/False if letter_guessed is a single letter that wasn't guessed before
     :rtype: boll."""
-    guessed_is_not_old = True
     str_of_guessed_letters = ""
-    # check if letter_guessed was not guessed all ready.
-    for letter in old_letters_guessed:
-        if letter == letter_guessed:
-            guessed_is_not_old = False
-            break
     # check if letter_guess is a single letter that is guessed fo the first time.
-    if check_valid_input(letter_guessed, old_letters_guessed) and guessed_is_not_old:
+    if check_valid_input(letter_guessed, old_letters_guessed):
         old_letters_guessed.append(letter_guessed.lower())
         return True
+    elif len(letter_guessed) != 0 and not letter_guessed.isalpha():
+        print("X")
+        return False
     else:
         # convert the list of old letters to a string.
         for n in old_letters_guessed:
@@ -174,7 +171,7 @@ def choose_word(file_path, index):
         word_count = len(words_list)
         # collect a word from the file.
         while index >= len(words_list):
-            index =- len(words_list)
+            index -= word_count
         chosen_word = words_list[index]
         return chosen_word
 
@@ -185,41 +182,72 @@ def hangman(file_path, index):
     :param: index for a word location in the file.
     :type file_path: str.
     :type index: int
-    :return: True if the player win the game 0r False if player los.
+    :return: String that indiceate
     :rtype: bool"""
     secret_word = choose_word(file_path, index)
     old_letter_guessed = []
     MAX_TRIES = 6
     num_of_tries = 0
     HANGMAN_PHOTOS = hangman_pictures()
+    print_hangman()
     print(HANGMAN_PHOTOS[num_of_tries])
     print(show_hidden_word(secret_word, old_letter_guessed))
     letter_guess = input("Enter your guess here: ")
     # The game will continue until player number of tries reach to the max of 6 tries.
-    while num_of_tries < MAX_TRIES:
+    while num_of_tries <= MAX_TRIES:
         # Check that the guess of the player is legal.
         if not try_update_letter_guessed(letter_guess, old_letter_guessed):
             letter_guess = input("Enter your guess here: ")
             continue
         # Check if the player is wining.
         if check_win(secret_word, old_letter_guessed):
-            return True
+            return secret_word + "\n" + "WIN"
         else:
             # Check if player guess is not correct.
             if letter_guess not in secret_word:
                 num_of_tries += 1
                 print(HANGMAN_PHOTOS[num_of_tries])
-            print(show_hidden_word(secret_word,old_letter_guessed))
+            print(show_hidden_word(secret_word, old_letter_guessed))
             letter_guess = input("Enter your guess here: ")
-    print(HANGMAN_PHOTOS[MAX_TRIES])
-    return False
+    print(HANGMAN_PHOTOS[-1])
+    return "LOSE"
+
+
+def collect_file_path():
+    """Function receive from the user the path of a file and check the path is correct.
+    :return: True if path for the file is correct or False if file is not found.
+    :rtype: bool"""
+    file_path = input("Enter please the file path: ")
+    while True:
+        try:
+            with open(file_path, "r"):
+                return file_path
+        except FileNotFoundError:
+            print("Cant find this file, please check the file path agine.")
+            file_path = input("Enter please the file path: ")
+            return False
+
+
+def collect_index():
+    """Function receive form the user index and convert it to a int type variable.
+    :return: index type int
+    :rtype: int"""
+    index = input("Enter please a index: ")
+    while True:
+        try:
+            index = int(index)
+            if index < 0:
+                return index * -1
+            return index
+        except ValueError:
+            print("You dont enter a whole number for index, please enter a legal number agine.")
+            index = input("Enter please a index: ")
 
 
 def main():
-    if hangman(r"C:\Users\lavi\DevOpsCourse\DevOpsCourse\python\camposil-self-py-course\self course\venv\Tasks\Hangman\Hangman\venv\words-file", 2):
-        print("you win")
-    else:
-        print("you los")
+    file_path = collect_file_path()
+    index = collect_index()
+    print(hangman(file_path, index))
 
 
 if __name__ == "__main__":
